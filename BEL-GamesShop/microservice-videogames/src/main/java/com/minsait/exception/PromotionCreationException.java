d25b6c26a3293361f38e5a3fa0bc9f438fd8a4ef
Brian Lopez <brlopezl@minsait.com>
2024-03-11 20:37:20 -0600

    errores

---
+++ BEL-GamesShop/microservice-videogames/src/main/java/com/minsait/exception/PromotionCreationException.java
@@ -0,0 +1,7 @@
+package com.minsait.exception;
+
+public class PromotionCreationException extends  Exception {
+    public PromotionCreationException(String message) {
+        super(message);
+    }
+}
