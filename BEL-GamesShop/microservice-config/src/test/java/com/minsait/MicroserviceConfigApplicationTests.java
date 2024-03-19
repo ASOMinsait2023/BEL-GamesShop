b01e1d80250cb3999df8c469d03f620cfc88e55a
Brian Lopez <brlopezl@minsait.com>
2024-03-18 18:10:08 -0600

    test update and config

---
+++ BEL-GamesShop/microservice-config/src/test/java/com/minsait/MicroserviceConfigApplicationTests.java
@@ -0,0 +1,18 @@
+package com.minsait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void main() {
+		MicroserviceConfigApplication.main(new String[]{});
+	}
+
+}
